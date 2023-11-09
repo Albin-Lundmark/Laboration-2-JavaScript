ac4222e0e19c9c1b98a886bf4a35be7a97eea4b1
Albin Lundmark <albin.lundmark@iths.se>
2023-11-09 23:39:36 +0100

    Påbörjat search(), ändrat navbar och dialog

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ closeModalBtn.addEventListener("click", () => {
   modal.close()
 })
 
+
+
 /* 2. Navbar */
 
 //Navbar försvinner ovanför skärmen när man scrollar
@@ -36,8 +38,22 @@ window.onscroll = () => {
   }
   scrollpos = currentScrollPos;
 }
+
+//Skapar en funktion för min searchbar i navbaren
+const search = () => {
+
+}
+
+//Hämtar searchbaren från HTML och lägger till en lyssnare
+const searchBar = document.querySelector('#search-bar').addEventListener('keydown', (event) => {
+  if (event.target.value === 'Enter') {
+    search()
+  }
+})
+
 /* 3. Variabler för fetch() */
-/* Här skapar jag en funktion med async/await som innehåller min fetch() för filmer som ska visas på sidan och använder mig av en callback för att hämta filmerna asynkront */
+
+/* Här skapar jag en funktion med async/await som innehåller hela min fetch() för filmer som ska visas på sidan och sedan använder jag mig av en callback för att hämta och visa upp filmerna asynkront */
 
 const getMovies = async () => {
   try {
@@ -47,8 +63,8 @@ const getMovies = async () => {
         accept: 'application/json',
         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZjU2ZDkzZGUxNGUwNmY0M2ZjYWZlYzBjMmVkZDA1YiIsInN1YiI6IjY1NDEwNTAxMzNhNTMzMDE0ZDQ2YjdlZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-VhFqWT1Gq2DibxvzyqFN2kDbqOFUC44V7EOvjLgU9Y'
       }
-    };
-    //Hämtar in varje section där filmerna ska visas i variabler
+    }
+    //Hämtar varje section från DOM där filmerna ska visas in i variabler
     const movieCarousel1 = document.querySelector('#movie-section-1')
     const movieCarousel2 = document.querySelector('#movie-section-2')
     const movieCarousel3 = document.querySelector('#movie-section-3')
@@ -58,11 +74,10 @@ const getMovies = async () => {
     const data = response.data
     //Loggar ut datan jag inhämtat för att se att det är rätt data
     console.log(data)
-    console.log(data.results[0].backdrop_path)
 
     //Skapar en variabel som mapar den inhämtade datan till en ny array som jag sedan kan plocka ut datan ifrån och visa upp
     const movieImgCar1 = await data.results.map((results) => {
-      return `<div><a href=""><img class="backdrop-image" alt="${results.original_title}" src="https://themoviedb.org/t/p/w220_and_h330_face${results.poster_path}"><p class="movie-title">${results.original_title}</p></a></div>`
+      return `<a href="https://themoviedb.org/movie/${results.id}"><img class="poster-image" loading="lazy" alt="${results.original_title}" src="https://themoviedb.org/t/p/w220_and_h330_face${results.poster_path}"><p class="movie-title">${results.title}</p></a>`
     })
 
     //Kör metoden join() med en innerHTML för att skapa nya element
@@ -72,7 +87,7 @@ const getMovies = async () => {
     const response2 = await axios.get('https://api.themoviedb.org/3/movie/popular?language=en-US&page=2', options)
     const data2 = response2.data
     const movieImgCar2 = await data2.results.map((results) => {
-      return `<a href=""><img class="backdrop-image" alt="${results.original_title}" src="https://themoviedb.org/t/p/w220_and_h330_face${results.poster_path}"><p class="movie-title">${results.original_title}</p></a>`
+      return `<a href="https://themoviedb.org/movie/${results.id}"><img class="poster-image" loading="lazy" alt="${results.title}" src="https://themoviedb.org/t/p/w220_and_h330_face${results.poster_path}"><p class="movie-title">${results.title}</p></a>`
     })
     movieCarousel2.innerHTML = movieImgCar2.join('')
 
@@ -80,7 +95,7 @@ const getMovies = async () => {
     const response3 = await axios.get('https://api.themoviedb.org/3/movie/popular?language=en-US&page=3', options)
     const data3 = response3.data
     const movieImgCar3 = await data3.results.map((results) => {
-      return `<a href=""><img class="backdrop-image" alt="${results.original_title}" src="https://themoviedb.org/t/p/w220_and_h330_face${results.poster_path}"><p class="movie-title">${results.original_title}</p></a>`
+      return `<a href="https://themoviedb.org/movie/${results.id}"><img class="poster-image" loading="lazy" alt="${results.title}" src="https://themoviedb.org/t/p/w220_and_h330_face${results.poster_path}"><p class="movie-title">${results.title}</p></a>`
     })
     movieCarousel3.innerHTML = movieImgCar3.join('')
   }
@@ -89,4 +104,5 @@ const getMovies = async () => {
     console.error('Fel:', error)
   }
 }
+//Kör min fetch() som ligger i funktionen getMovies()
 getMovies()
