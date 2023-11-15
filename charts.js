0d8e519664ed6e54b06d3042c7d952ebd8a07991
Albin Lundmark <albin.lundmark@iths.se>
2023-11-15 14:26:28 +0100

    Lagt till chart.js i projektet

--- charts.js
+++ charts.js
@@ -138,11 +138,7 @@ const searchBar = document.querySelector('#search-bar').addEventListener('keydow
   }
 })
 
-/* 3. Variabler för fetch() */
-
-/* Här skapar jag en funktion med async/await som innehåller hela min fetch() för filmer som ska visas på sidan och sedan använder jag mig av en callback för att hämta och visa upp filmerna asynkront */
-
-const getMovies = async () => {
+const chart = async () => {
   try {
     //Skapar en variabel som innehåller API-nyckel för att kunna komma åt all data från det API jag har valt
     const options = {
@@ -151,40 +147,4 @@ const getMovies = async () => {
         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZjU2ZDkzZGUxNGUwNmY0M2ZjYWZlYzBjMmVkZDA1YiIsInN1YiI6IjY1NDEwNTAxMzNhNTMzMDE0ZDQ2YjdlZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-VhFqWT1Gq2DibxvzyqFN2kDbqOFUC44V7EOvjLgU9Y'
       }
     }
-    //Hämtar varje section från DOM där filmerna ska visas in i variabler
-    const movieCarousel1 = document.querySelector('#movie-section-1')
-    const movieCarousel2 = document.querySelector('#movie-section-2')
-    const movieCarousel3 = document.querySelector('#movie-section-3')
-
-    //Använder mig av axios med await för min GET till önskat API, använder options variabeln från ovan där jag lagt in API nyckel
-    const response = await axios.get('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
-    const data = response.data
-    //Loggar ut datan jag inhämtat för att se att det är rätt data
-    console.log(data)
-
-    //Mapar den inhämtade datan till en ny array som jag visar upp genom innerHTML i den första karusellen på sidan
-    movieCarousel1.innerHTML = data.results.map((results) => {
-      return `<a href="https://themoviedb.org/movie/${results.id}"><img class="poster-image" loading="lazy" alt="${results.original_title}" src="https://themoviedb.org/t/p/w220_and_h330_face${results.poster_path}"><p class="movie-title">${results.title}</p></a>`
-    }).join('')
-
-    //Samma som ovan nämnda kod men för karusell 2
-    const response2 = await axios.get('https://api.themoviedb.org/3/movie/popular?language=en-US&page=2', options)
-    const data2 = response2.data
-    movieCarousel2.innerHTML = data2.results.map((result) => {
-      return `<a href="https://themoviedb.org/movie/${result.id}"><img class="poster-image" loading="lazy" alt="${result.title}" src="https://themoviedb.org/t/p/w220_and_h330_face${result.poster_path}"><p class="movie-title">${result.title}</p></a>`
-    }).join('')
-
-    //Och här för karusell 3
-    const response3 = await axios.get('https://api.themoviedb.org/3/movie/popular?language=en-US&page=3', options)
-    const data3 = response3.data
-    movieCarousel3.innerHTML = data3.results.map((results) => {
-      return `<a href="https://themoviedb.org/movie/${results.id}"><img class="poster-image" loading="lazy" alt="${results.title}" src="https://themoviedb.org/t/p/w220_and_h330_face${results.poster_path}"><p class="movie-title">${results.title}</p></a>`
-    }).join('')
   }
-  //Loggar fel för att bättre kunna förstå om något inte går som jag vill
-  catch (error) {
-    console.error('Fel:', error)
-  }
-}
-//Kör min fetch() som ligger i funktionen getMovies()
-getMovies()
